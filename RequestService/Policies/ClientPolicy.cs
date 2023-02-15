9f2f4e4676bed37d18f39a345949a63dcf05a50f
Douglas <dglandre@gmail.com>
2023-02-15 00:25:27 -0300

    Policy 3: Retry Exponential

--- RequestService/Policies/ClientPolicy.cs
+++ RequestService/Policies/ClientPolicy.cs
@@ -7,7 +7,7 @@ namespace RequestService.Policies
     {
         public AsyncRetryPolicy<HttpResponseMessage> ImmediateHttpRetry { get; }
         public AsyncRetryPolicy<HttpResponseMessage> LinearHttpRetry { get; }
-
+        public AsyncRetryPolicy<HttpResponseMessage> ExponentialHttpRetry { get; }
         
         public ClientPolicy()
         {
@@ -18,6 +18,10 @@ namespace RequestService.Policies
             LinearHttpRetry = Policy.HandleResult<HttpResponseMessage>(
                 res => !res.IsSuccessStatusCode)
                 .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(3));
+            
+            ExponentialHttpRetry = Policy.HandleResult<HttpResponseMessage>(
+                res => !res.IsSuccessStatusCode)
+                .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
         }
     }
 }
